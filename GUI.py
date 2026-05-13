86574d18244ca84502c5226e38f4d37beffa586d
Alvarez2796 <arialvarez@csumb.edu>
2026-05-13 15:21:14 -0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,7 +1,9 @@
 import sys
-from PySide6.QtWidgets import (QApplication,QWidget, QLabel, QLineEdit, QTextBrowser,
-                                QComboBox, QPushButton, QHBoxLayout, QDialog, QVBoxLayout)
+from PySide6.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton,
+                                QVBoxLayout)
 from PySide6.QtCore import Slot
+
+from Decrypter import connect
 
 my_app = QApplication([])
 
@@ -10,19 +12,22 @@ class mainScreen(QWidget):
         super().__init__()
         my_title = QLabel("Encrypter/Decrypter")
 
-        self.userline = QLineEdit("Please Enter A Search Term: ")
-        self.userline.setMinimumWidth(250)
-        self.userline.selectAll()
+        self.userline = QLineEdit()
+        self.userline.setPlaceholderText("Image to hide")
+
+        self.cryptline = QLineEdit()
+        self.cryptline.setPlaceholderText("cover img")
+    
 
         button = QPushButton("Enter")
         self.label = QLabel("")
 
         button.clicked.connect(self.open)
-        self.userline.returnPressed.connect(self.open)
 
         vbox = QVBoxLayout()
         vbox.addWidget(my_title)
         vbox.addWidget(self.userline)
+        vbox.addWidget(self.cryptline)
         vbox.addWidget(button)
         vbox.addWidget(self.label)
 
@@ -30,11 +35,21 @@ class mainScreen(QWidget):
     
     @Slot()
     def open(self):
-        your_input = self.userline.text()
-        self.label.setText(f"Here's what you entered: {your_input}")
+        p = self.hiddenLine.text().strip()
+        p2 = self.carrierLine.text().strip()
+
+        if not p or not p2:
+            self.label.setText("Enter image desc.")
+            return
+        
+        self.label.setText("Working")
+        try:
+            connect(p, p2)
+            self.label.setText("Finished")
+        except Exception as e:
+            self.label.setText(f"Fail: {e}")
 
 
 my_win = mainScreen()
 my_win.show()
-
 sys.exit(my_app.exec())
